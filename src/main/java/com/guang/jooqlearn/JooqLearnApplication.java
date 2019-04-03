5a146fcc6eb7a2cf2f5462645630c665bbe1aa0c
guang <guang@xingren.com>
2019-04-03 15:57:24 +0800

    Initial commit

---
+++ src/main/java/com/guang/jooqlearn/JooqLearnApplication.java
@@ -0,0 +1,13 @@
+package com.guang.jooqlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqLearnApplication.class, args);
+    }
+
+}
